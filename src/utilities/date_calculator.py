622713bff63f5d1a754467e19b47e4b3cc583332
Arthur Vargas <ahvargas92@gmail.com>
2020-05-20 00:00:35 -0400

    beginning to stub out classes

---
+++ src/utilities/date_calculator.py
@@ -0,0 +1,10 @@
+##==============================================================
+## Module date_calculator.py
+##==============================================================
+"""
+date_calculator.py
+Module providing custom calculations for specific units of time (days, hours, minutes, etc.)
+Description
+-----------
+The date_calculator class provides datetime calculations for past and future events
+"""
